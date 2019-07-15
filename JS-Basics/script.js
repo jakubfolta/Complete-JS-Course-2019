aba40d9928f64d8f4a51bdcda5727996fab2973e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-07-15 22:55:56 +0100

    Add next lectures.

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -59,3 +59,10 @@ console.log(now - 2);
 // Logical operators
 var johnOlder = ageJohn > ageMark;
 console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'John');
+var x;
+console.log(typeof x);
