3c94adf04ad104c697c49d88b7d9992d841aeb36
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-07-15 22:50:35 +0100

    Add next lectures.

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -40,3 +40,22 @@ console.log('His last name is ' + lastName + '.')
 /********************************
 * Basic operators
 */
+
+// Math operators
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+console.log(now - 2);
+
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
