6207b80b00818c8a1358d325fe60ddecda113687
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-07-15 22:33:30 +0100

    Add next lectures.

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -1,3 +1,4 @@
+/*****
 var firstName = 'John';
 console.log(firstName);
 
@@ -7,10 +8,35 @@ var age = 26;
 var fullAge = true;
 console.log(fullAge);
 
-var job;fsd
+var job;
 
 console.log(job);
 
 job = 'Teacher';
 console.log(job);
 console.log(firstName + lastName + fullAge);
+*/
+/*
+var firstName = 'John';
+var age = 27;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name? ');
+console.log('His last name is ' + lastName + '.')
+*/
+
+/********************************
+* Basic operators
+*/
