d45b48f5654a811b8ef0ddd64bf88694cec11a5f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-01 07:07:16 +0100

    Add finished coding challenge 4.

--- JS-Basics/basics.js
+++ JS-Basics/basics.js
@@ -557,6 +557,7 @@ console.log(jane);
 ******************************************* OBJECTS AND METHODS *************************************
 */
 
+/*
 var john = {
     firstName: 'John',
     lastName: 'Smith',
@@ -564,14 +565,100 @@ var john = {
     family: ['Jane', 'Bob', 'Tifa', 'Cloud'],
     job: 'teacher',
     calcAge: function() {
-        return 2019 - this.birthYear;
+        this.age = 2019 - this.birthYear;
     }
 };
 
-console.log(john.calcAge()); // or pass the number
+console.log(john);
 
-john.age = john.calcAge();
+john.calcAge();
+console.log(john);
+*/
 
+/*
+******************************************* CODING CHALLENGE 4 *************************************
+*/
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+GOOD LUCK 😀
+*/
+
+var john = {
+    name: 'John',
+    mass: 78,
+    height: 1.89,
+    calcBMI: function() {
+        return this.BMI = this.mass / this.height^2;
+    }
+};
+
+var mark = {
+    name: 'Mark',
+    mass: 92,
+    height: 1.97,
+    calcBMI: function() {
+        return this.BMI = this.mass / this.height^2;
+    }
+};
+
+console.log(mark.name + '\'s BMI is ' + mark.calcBMI());
+console.log(john.name + '\'s BMI is ' + john.calcBMI());
+
+if (john.BMI > mark.BMI) {
+    console.log('John has higher BMI than Mark.');
+} else if (john.BMI < mark.BMI) {
+    console.log('Mark has higher BMI than John.');
+} else {
+    console.log('John and Mark have the same BMI!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
 
 
 
