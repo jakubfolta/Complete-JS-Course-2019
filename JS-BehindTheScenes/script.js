ffdf35f7d91a6d31472e58d1af8a5b00e3ce337b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-02 23:46:33 +0100

    Continue lecture 'Hoisting'.

--- JS-BehindTheScenes/script.js
+++ JS-BehindTheScenes/script.js
@@ -1,17 +1,21 @@
 /*
 ******************************************* Hoisting *************************************
 */
+
+// functions
 calculateAge(1987); // function hoisted as it is function declaration
-//retirement(56); // doesn't work as it is function expression
+// retirement(56); // doesn't work as it is function expression
 
 function calculateAge(year) {
     console.log(2019 - year);
 }
 
-//calculateAge(1987);
+// calculateAge(1987);
 
 var retirement = function(year) {
     console.log(65 - year);
 }
 
 retirement(56);
+
+// variables
