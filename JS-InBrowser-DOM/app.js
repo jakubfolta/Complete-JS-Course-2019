33d5b133f1fb8f95319317e07676d2c139373ed3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-03 17:49:14 +0100

    Continue lecture 'JS in browser - DOM'

--- JS-InBrowser-DOM/app.js
+++ JS-InBrowser-DOM/app.js
@@ -18,18 +18,34 @@ activePlayer = 0;
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; // setter
 
-var x = document.querySelector('#current-0').textContent;  // getter
-console.log(x);
+
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() { // anonymous function, can be used only here inside addEventListener function
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
     // 2. Display the score
-    document.querySelector('#current-' + activePlayer).textContent = dice;
-    // 3. Update the score only if rolled number was not one
+    var diceDOM = document.querySelector('.dice');
+    var currentDOM = document.querySelector('#current-' + activePlayer);
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
+    // 3. Update the score only if rolled number was not one
+    if (dice !== 1) {
+        roundScore += dice;
+        currentDOM.textContent = roundScore;
+    }   else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        currentDOM.textContent = 0;
+
+    }
 });
 
 
