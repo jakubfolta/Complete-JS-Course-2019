c0e586b242bec21e5fcf5aface91c1410ccce4ea
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-06 22:37:29 +0100

    Refactor coding challenge 6.1 and start challenge 6.2.

--- JS-InBrowser-PigGame/Coding-challenge-6.2/script.js
+++ JS-InBrowser-PigGame/Coding-challenge-6.2/script.js
@@ -17,31 +17,76 @@ Change the game to follow these rules:
 */
 
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, maxPoints, prevScore;
 
-init();
 
+var app = {
+    init: function() {
+        scores = [0, 0];
+        roundScore = 0;
+        activePlayer = 0;
+        gamePlaying = true;
+        maxPoints = 100;
+        prevScore = 0;
+
+        document.querySelector('.player-0-panel').classList.remove('winner');
+        document.querySelector('.player-1-panel').classList.remove('winner');
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-0-panel').classList.add('active');
+        document.querySelector('.player-1-panel').classList.remove('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.getElementById('name-0').textContent = 'Player 1';
+        document.getElementById('name-1').textContent = 'Player 2';
+    },
+    restart: function() {
+        return 0;
+    }
+};
+
+
+// INITIALIZE GAME
+app.init();
+
+// ROLL BUTTON
 document.querySelector('.btn-roll').addEventListener('click', function() { // anonymous function, can be used only here inside addEventListener function
     if (gamePlaying) {
         // 1. Random number
         var dice = Math.floor(Math.random() * 6) + 1;
-        var currentDOM = currentDOM = document.querySelector('#current-' + activePlayer);
+        var currentDOM = document.querySelector('#current-' + activePlayer);
+        console.log(dice, prevScore);
 
         // 2. Display the score
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = 'JS-InBrowser-PigGame/dice-' + dice + '.png';
+        diceDOM.src = 'dice-' + dice + '.png';
 
         // 3. Update the score only if rolled number was not one
-        if (dice !== 1) {
-            roundScore += dice;
-            currentDOM.textContent = roundScore;
-        }   else {
-            nextPlayer();
+        switch(true) {
+            case dice === 1:
+                prevScore = dice;
+                nextPlayer();
+                break;
+            case dice === 6 && prevScore === 6:
+                scores[activePlayer] = app.restart();
+                document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+                prevScore = app.restart();
+                nextPlayer();
+                break;
+            case dice !== 1:
+                roundScore += dice;
+                prevScore = dice;
+                currentDOM.textContent = roundScore;
         }
     }
 });
 
+// HOLD BUTTON
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
         // Add current score to global score
@@ -49,7 +94,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= maxPoints) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.active').classList.add('winner');
             document.querySelector('.dice').style.display = 'none';
@@ -57,45 +102,31 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
             gamePlaying = false;
         } else {
             // Reset current score and change player
+            prevScore = app.restart();
             nextPlayer();
         }
     }
 });
 
-document.querySelector('.btn-new').addEventListener('click', init);
+// NEW GAME BUTTON
+document.querySelector('.btn-new').addEventListener('click', app.init);
+
 
 function nextPlayer() {
-    var currentDOM = currentDOM = document.querySelector('#current-' + activePlayer);
+    var currentDOM = document.querySelector('#current-' + activePlayer);
     currentDOM.textContent = 0;
+    roundScore = app.restart();
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
 
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 };
 
-function init() {
-    scores = [0, 0];
-    roundScore = 0;
-    activePlayer = 0;
-    gamePlaying = true;
 
-    document.querySelector('.player-0-panel').classList.remove('winner');
-    document.querySelector('.player-1-panel').classList.remove('winner');
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.add('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
 
-    document.querySelector('.dice').style.display = 'none';
 
-    document.getElementById('score-0').textContent = '0';
-    document.getElementById('score-1').textContent = '0';
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
-};
+
 
 
 
