795a597b9df3ef93a26e9b8a3042d788d9608b9c
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-10 20:03:49 +0100

    Solidyfy knowledge.

--- JS-Advanced-ObjectsFunctions/script.js
+++ JS-Advanced-ObjectsFunctions/script.js
@@ -8,6 +8,12 @@ var john = {
     job: 'teacher'
 };
 
+var AvalancheMember = function(name, hasMateria, weapon) {
+    this.name = name;
+    this.hasMateria = hasMateria;
+    this.weapon = weapon;
+};
+
 var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
@@ -21,10 +27,22 @@ Person.prototype.calculateAge = function() {
 
 Person.prototype.lastName = 'Smith';
 
+AvalancheMember.prototype.calcAge = function(yearOfBirth) {
+    console.log(2019 - yearOfBirth);
+};
+
+
+// creating instances
 var john = new Person('John', 1987, 'teacher');
 var jane = new Person('Jane', 1990, 'designer');
 var cloud = new Person('Cloud', 1989, 'soldier')
 
+var tidus = new AvalancheMember('Cloud', true, 'Ultima Weapon');
+var tifa = new AvalancheMember('Tifa', false, 'Thunder Gloves');
+var barret = new AvalancheMember('Barret', true, 'Plasma Cannon');
+
+
+// inheritance
 john.calculateAge();
 jane.calculateAge();
 cloud.calculateAge();
@@ -32,6 +50,37 @@ cloud.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(cloud.lastName);
+
+
+barret.calcAge(1985);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
