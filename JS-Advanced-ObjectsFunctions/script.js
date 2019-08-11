de6fcd2483eb74dcebf6181488904cd6776a5b9a
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-11 12:37:19 +0100

    Continue 'Passing functions as arguments' lecture.

--- JS-Advanced-ObjectsFunctions/script.js
+++ JS-Advanced-ObjectsFunctions/script.js
@@ -136,7 +136,28 @@ console.log(obj.name);
 ******************************************* Passing functions as arguments *************************************
 */
 
+var years = [1998, 1876, 1979, 2015, 1936];
 
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for(var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2019 - el;
+}
+function isFullAge(el) {
+    return el >= 18;
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(ages);
+console.log(fullAges);
 
 
 
