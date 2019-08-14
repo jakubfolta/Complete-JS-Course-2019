b610bc2f7237cdaca30c89f237c6f4296a51c4cf
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-14 05:43:10 +0100

    Continue 'Budget App Project' lecture.

--- JS-BudgetAppProject/app.js
+++ JS-BudgetAppProject/app.js
@@ -15,15 +15,20 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-document.querySelector('.add__btn').addEventSelector('click', function() {
+    document.querySelector('.add__btn').addEventSelector('click', function() {
 
-    // 1. Get the filled input data
-    // 2. Add the item to the budget controller
-    // 3. Add the item to the UI
-    // 4. Calculate the budget
-    // 5. Display the budget in the UI
-    
-})
+        // 1. Get the filled input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget in the UI
 
+    })
+
+    document.addEventListener('keypress', function() {
+
+
+
+    })
 
 })(budgetController, UIController);
