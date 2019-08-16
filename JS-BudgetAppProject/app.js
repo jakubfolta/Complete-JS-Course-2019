bc63d857b548caf246cb4a9fedac629acd76ecee
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-16 22:18:47 +0100

    Solidifying knowledge

--- JS-BudgetAppProject/app.js
+++ JS-BudgetAppProject/app.js
@@ -1,17 +1,6 @@
 // BUDGETY CONTROLLER
 var budgetController = (function() {
-    var Expense = function(id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-
-    var Income = function(id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-
+    
 })();
 
 
