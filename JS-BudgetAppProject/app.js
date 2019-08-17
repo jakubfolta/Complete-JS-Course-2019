74d20588d4ee447c3072e2fd1e2408fa4d629a91
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-17 23:49:27 +0100

    Add new item - Budget App.

--- JS-BudgetAppProject/app.js
+++ JS-BudgetAppProject/app.js
@@ -95,8 +95,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         console.log(input);
 
         // 2. Add the item to the budget controller
+        budgetCtrl.addItem(input.getInput.type, input.getInput.description, input.getInput.value)
         // 3. Add the item to the UI
+
         // 4. Calculate the budget
+
         // 5. Display the budget in the UI
     };
 
@@ -105,7 +108,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             console.log('Initialize Listeners...')
             setupEventListeners();
         }
-    };
+    };+
 
 
 })(budgetController, UIController);
