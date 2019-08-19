c07c94dc21e2b253fa832de240345f8240be373f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-19 05:20:57 +0100

    Add list item to the UI - Budget App.

--- JS-BudgetAppProject/app.js
+++ JS-BudgetAppProject/app.js
@@ -71,12 +71,12 @@ var UIController = (function() {
             };
         },
 
-        addListItem = function(obj, type) {
+        addListItem: function(obj, type) {
             var html, newHtml;
 
             // Create HTML string with placeholder text
             if (type === 'inc') {
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">&description&</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
@@ -129,7 +129,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
-        addListItem(newItem, input.type);
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
