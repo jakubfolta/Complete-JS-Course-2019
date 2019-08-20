c29ab5c9b0c8d8f20f1f97aae50c88357e44b481
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-20 21:40:48 +0100

    Clearing out input fields - solidifying.

--- JS-BudgetAppProject/app.js
+++ JS-BudgetAppProject/app.js
@@ -97,17 +97,7 @@ var UIController = (function() {
         },
 
         clearFields: function() {
-            var fields, fieldsArr;
-
-            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
-
-            fieldsArr = Array.prototype.slice.call(fields);
-
-            fieldsArr.forEach(function(current, index, array) {
-                current.value = '';
-            });
-
-            fieldsArr[0].focus();
+            document
         },
 
         getDOMstrings: function() {
