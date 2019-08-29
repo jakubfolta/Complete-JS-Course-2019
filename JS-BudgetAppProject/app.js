71070dfee4ccd9f8aad28d3c12bf91b3f41a86b9
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-08-29 09:05:25 +0100

    Delete item from the UI, solidifying knowledge - Budget App.

--- JS-BudgetAppProject/app.js
+++ JS-BudgetAppProject/app.js
@@ -156,11 +156,6 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
-        deleteListItem: function(selectorID) {
-            var el = document.getElementById(selectorID);
-
-            el.parentNode.removeChild(el);
-        },
 
         clearFields: function() {
             var fields, fieldsArr;
@@ -261,7 +256,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
-            UICtrl.deleteListItem(itemID);
+
 
             // 3. Update and show the budget
 
