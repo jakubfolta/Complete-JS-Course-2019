bb86ed6970479361e61e074d8a813135a601ea4a
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-02 16:03:20 +0100

    Continue lecture - JS Next Generation ES6 - Destructuring.

--- JS-NextGenerationES6/script.js
+++ JS-NextGenerationES6/script.js
@@ -195,9 +195,15 @@ new Person('Cass').myFriends(friends);
 // Lecture: Destructuring
 /////////////////////////
 
+// ES5
+var john = ['John', 26];
+//var name = john[0];
+//var age = john[1];
 
-
-
+//ES6
+const [name, age] = john;
+console.log(name);
+console.log(age);
 
 
 
