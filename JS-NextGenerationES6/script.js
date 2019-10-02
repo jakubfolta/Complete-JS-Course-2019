3c78d07ab2d2c3b34b3138fa0e4c21f3086a0e0c
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-02 17:55:55 +0100

    Continue lecture - JS Next Generation ES6 - Arrays, new methods.

--- JS-NextGenerationES6/script.js
+++ JS-NextGenerationES6/script.js
@@ -284,6 +284,12 @@ console.log(full);
 console.log(full.indexOf(true));
 console.log(ages[full.indexOf(true)]);
 
+// ES6
+
+console.log(ages.findIndex(cur => cur >= 18));
+
+console.log(ages.find(cur => cur >= 18));
+
 
 
 
