f5dfe2a3085bed074568ea1a36384d1dd0c995e7
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-02 15:08:59 +0100

    Continue lecture - JS Next Generation ES6 - Arrow functions.

--- JS-NextGenerationES6/script.js
+++ JS-NextGenerationES6/script.js
@@ -129,9 +129,34 @@ console.log(ages6);
 // Lecture: Arrow functions 2
 /////////////////////////
 
+// ES5
+var box5 = {
+	color: 'green',
+	position: 1,
+	clickMe: function() {
+	
+		var self = this;
+		document.querySelector('.green').addEventListener('click', function() {
+		var str = 'This is box number ' + self.position + ' and it is ' + self.color + '.';
+		alert(str);
+		})
+	}
+}
+//box5.clickMe();
 
 
-
+var box6 = {
+	color: 'green',
+	position: 1,
+	clickMe: function() {
+	
+		document.querySelector('.green').addEventListener('click', () => {
+		var str = `This is box number ${this.position} and it is ${this.color}.`;
+		alert(str);
+		});
+	}
+}
+box6.clickMe();
 
 
 
