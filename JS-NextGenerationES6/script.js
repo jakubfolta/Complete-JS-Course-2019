7388530d84b30a2336e75ef57c851ebbdfaab61b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-08 23:35:36 +0100

    Continue coding challenge.

--- JS-NextGenerationES6/script.js
+++ JS-NextGenerationES6/script.js
@@ -586,100 +586,132 @@ HINT: Use some of the ES6 features: classes, subclasses, template strings, defau
 
 */
 
-// Create class for town element and subclasses for parks and streets
-class TownElement {
-	constructor(name, year) {
-		this.name = name;
-		this.year = year;
+const townData = (() => {
+	// Create class for town element and subclasses for parks and streets
+	class TownElement {
+		constructor(name, year) {
+			this.name = name;
+			this.year = year;
+		}
+
+		calcAge() {
+			const age = new Date().getFullYear() - this.year;
+			return age;
+		}
 	}
-	
-	calcAge() {
-		const age = new Date().getFullYear() - this.year;
-		return age;
-	}
-}
 
-class Park extends TownElement {
-	constructor (name, year, trees, area) {
-		super (name, year);
-		this.trees = trees;
-		this.area = area; //km2
-		this.age = this.calcAge();
-	}
-	
-	calcTreeDensity() {
-		const density = this.trees / this.area;
-		console.log(`${this.name} has a density of ${density} trees per square km.`)
-	}	
-}
+	class Park extends TownElement {
+		constructor (name, year, trees, area) {
+			super (name, year);
+			this.trees = trees;
+			this.area = area; //km2
+			this.age = this.calcAge();
+		}
 
-class Street extends TownElement {
-	constructor (name, year, length, size = 'normal') {
-		super (name, year);
-		this.length = length;
-		this.size = size;
-	}
-}
-
-// Set all items inside map
-const parks = new Map();
-parks.set('park1', new Park('Green Park', 1987, 1290, 10));
-parks.set('park2', new Park('Oak Park', 1968, 1200, 20));
-parks.set('park3', new Park('Revolution Park', 1948, 978, 15));
-
-const streets = new Map();
-streets.set('street1', new Street('Jones Street', 1998, 47, 'big'));
-streets.set('street2', new Street('Abraham Street', 1948, 27, 'small'));
-streets.set('street3', new Street('May Street', 2006, 34));
-streets.set('street4', new Street('Roy Street', 2009, 13, 'tiny'));
-
-// Create function to check average park age
-const averParkAge = element => {
-	let totalAge = 0;
-	for (let [key, value] of element.entries()) {
-		if (key.startsWith('park')) {
-			totalAge += value.age;
+		calcTreeDensity() {
+			const density = this.trees / this.area;
+			console.log(`${this.name} has a density of ${density} trees per square km.`)
 		}	
 	}
-	console.log(`Average year of each park is ${Math.floor(totalAge / element.size)}.`);
-}
 
-// Create function to display tree density of each park
-const displayTreeDensity = map => 
-	map.forEach(cur => cur.calcTreeDensity());
-
-// Display park which has more tha 1000 trees
-const displayMoreThan1000 = map => {
-	let ParkName = new Map();
-	
-	for (let [key, value] of map.entries()) {
-		if (value.trees >= 1000) {
-			ParkName.set(value.name, value.trees);
-		} else continue
+	class Street extends TownElement {
+		constructor (name, year, length, size = 'normal') {
+			super (name, year);
+			this.length = length;
+			this.size = size;
+		}
 	}
-	console.log('Parks with more than 1000 trees:');
-	for (let [key, value] of ParkName) {
-	console.log(`${key}: ${value} trees.`);
+
+	// Set all items inside map
+	const parks = new Map();
+	parks.set('park1', new Park('Green Park', 1987, 1290, 10));
+	parks.set('park2', new Park('Oak Park', 1968, 1200, 20));
+	parks.set('park3', new Park('Revolution Park', 1948, 978, 15));
+
+	const streets = new Map();
+	streets.set('street1', new Street('Jones Street', 1998, 47, 'big'));
+	streets.set('street2', new Street('Abraham Street', 1948, 27, 'small'));
+	streets.set('street3', new Street('May Street', 2006, 34));
+	streets.set('street4', new Street('Roy Street', 2009, 13, 'tiny'));
+
+	const averParkAge = element => {
+		let totalAge = 0;
+		for (let [key, value] of element.entries()) {
+			if (key.startsWith('park')) {
+				totalAge += value.age;
+			}	
+		}
+		console.log(`Average year of each park is ${Math.floor(totalAge / element.size)}.`);
 	}
-}
 
-const treesReport = () => {
-	console.log('------Park Report-------');
-	
-	// Display average park age
-	averParkAge(parks);
-	
-	// Display tree density in each park
-	displayTreeDensity(parks);
-	
-	// Display parks with more than 1000 trees
-	displayMoreThan1000(parks);
-}
+	const displayTreeDensity = map => 
+		map.forEach(cur => cur.calcTreeDensity());
 
-const streetsReport = () => {
-	console.log('------Streets Report-------');
-	
-}
+	const displayMoreThan1000 = map => {
+		let ParkName = new Map();
+
+		for (let [key, value] of map.entries()) {
+			if (value.trees >= 1000) {
+				ParkName.set(value.name, value.trees);
+			} else continue
+		}
+		console.log('Parks with more than 1000 trees:');
+		for (let [key, value] of ParkName) {
+		console.log(`${key}: ${value} trees.`);
+		}
+	}
+
+	const calculateStreetsLegths = map => {
+		let lengths = [];
+		let totalLength;
+
+		for (let [key, value] of map.entries()) {
+			lengths.push(value.length);
+		}
+
+		totalLength = lengths.reduce((prev, cur) => prev + cur);
+
+		return [totalLength, totalLength / lengths.length] 
+	}
+
+	const displayStreetData = map => {
+		const [totalLength, average] = calculateStreetsLegths(streets); // Use of ES6 destructuring
+
+		for (let [key, value] of map.entries()) {
+			console.log(`${value.name}'s size classification is: ${value.size}. `)
+		}
+
+		console.log(`Total length of all streets is ${totalLength} and average length is ${average} .`)
+	}
+
+	// REPORTS
+	const treesReport = () => {
+		console.log('------Park Report-------');
+
+		// Display average park age
+		averParkAge(parks);
+
+		// Display tree density in each park
+		displayTreeDensity(parks);
+
+		// Display parks with more than 1000 trees
+		displayMoreThan1000(parks);
+	}
+
+	const streetsReport = () => {
+		console.log('------Streets Report-------');
+
+		// Display size classification, total length and average length of all streets
+		displayStreetData(streets);
+	}
+
+
+	// Display annual report
+	const displayReport = () => {
+		treesReport();
+		streetsReport();
+	}
+})();
 
 
 
@@ -688,70 +720,9 @@ const streetsReport = () => {
 
 
 
-// Display annual report
-const displayReport = () => {
-	treesReport();
-	streetsReport();
-}
 
 
 displayReport();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
