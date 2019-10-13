779082f32c40b6a443638146d9d41e655be6a3b2
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-13 13:27:30 +0100

    Continue forkify project - first API calls.

--- JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
+++ JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
@@ -5,12 +5,15 @@ import axios from 'axios';
 async function getResults(query) {
 	const key = '36d561b9fe597511d1bb317b56c8717d';
 	const proxy = 'https://cors-anywhere.herokuapp.com/';
-	
+	try {
 	const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
 	
 	const recipes = result.data.recipes;
 	
 	console.log(recipes);
+	} catch (err) {
+		alert (err);
+	}
 	
 }
 
