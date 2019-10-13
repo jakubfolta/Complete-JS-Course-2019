d9951c8b8dd30df814160e9d361102aede4004c3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-13 13:16:13 +0100

    Continue forkify project - first API calls.

--- JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
+++ JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
@@ -1,1 +1,15 @@
-//36d561b9fe597511d1bb317b56c8717d
+import axios from 'axios';
+
+// https://www.food2fork.com/api/search
+
+async function getResults(query) {
+	const key = '36d561b9fe597511d1bb317b56c8717d';
+	const proxy = 'https://cors-anywhere.herokuapp.com/';
+	
+	const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+	
+	console.log(result);
+	
+}
+
+getResults('tuna');
