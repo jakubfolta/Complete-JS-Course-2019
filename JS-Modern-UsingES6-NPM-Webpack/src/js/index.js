d08116c50c26fe01bf1198ffd8d89f1510831893
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-14 17:38:30 +0100

    Continue forkify project - first API calls.

--- JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
+++ JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-async function getResults(query) {
+async function getResultsRecipes(query) {
 	const key = '36d561b9fe597511d1bb317b56c8717d';
 	const proxy = 'https://cors-anywhere.herokuapp.com/';
 	
@@ -15,4 +15,24 @@ async function getResults(query) {
 	}
 }
 
-getResults('tuna');
+getResultsRecipes('tuna');
+
+async function getResults(query) {
+	const key = '011dd9b62094380079400d494066f431';
+	const proxy = 'https://cors-anywhere.herokuapp.com/';
+	
+	try{
+		const result = await axios(`${proxy}https://chicken-coop.p.rapidapi.com/games?title=${query}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "chicken-coop.p.rapidapi.com",
+		"x-rapidapi-key": "c39604baadmshcabd6e32bb7d9c0p1e3b1fjsnb9154a318acd"
+	}
+})
+		console.log(result);
+	} catch(err) {
+		console.log(err);
+	}
+}
+
+getResults('witcher');
