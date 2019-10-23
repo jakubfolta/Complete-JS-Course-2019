b84000af3974f8bd569441f87b170d7993c5fafa
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-23 02:01:04 +0100

    Continue forkify project - building the search model.

--- JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
+++ JS-Modern-UsingES6-NPM-Webpack/src/js/index.js
@@ -1,4 +1,7 @@
-import Search from './models/Search';
+import {Search} from './models/Search';
+import {markHoliday} from './models/Search';
 
 const search = new Search('pizza');
 search.getResults();
+
+console.log(markHoliday.bills);
