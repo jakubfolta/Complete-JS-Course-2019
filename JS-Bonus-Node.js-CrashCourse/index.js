d3fe571d5fa03039194b3657bf5f65d54e2147a8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-10 02:56:41 +0000

    Build simple Laptop store app with Node.js - continue.

--- JS-Bonus-Node.js-CrashCourse/index.js
+++ JS-Bonus-Node.js-CrashCourse/index.js
@@ -22,14 +22,15 @@ const server = http.createServer((req, res) => {
 
         fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
             const laptop = laptopData[id];
-            let output = data.replace('{%PRODUCTNAME%}', laptop.productName);
-            let output = output.replace('{%PRICE%}', laptop.price);
-            let output = output.replace('{%SCREEN%}', laptop.screen);
-            let output = output.replace('{%CPU%}', laptop.cpu);
-            let output = output.replace('{%STORAGE%}', laptop.storage);
-            let output = output.replace('{%RAM%}', laptop.ram);
-            let output = output.replace('{%DESCRIPTION%}', laptop.description);
-        })
+            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+            output = output.replace(/{%PRICE%}/g, laptop.price);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%CPU%}/g, laptop.cpu);
+            output = output.replace(/{%STORAGE%}/g, laptop.storage);
+            output = output.replace(/{%RAM%}/g, laptop.ram);
+            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            res.end(output);
+        });
     }
 
     else {
