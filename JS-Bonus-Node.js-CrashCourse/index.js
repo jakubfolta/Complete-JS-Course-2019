a8b0c4b7c98d8cca12c9576d73e15a2cf7e12c32
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-10 02:19:41 +0000

    Build simple Laptop store app with Node.js.

--- JS-Bonus-Node.js-CrashCourse/index.js
+++ JS-Bonus-Node.js-CrashCourse/index.js
@@ -8,22 +8,25 @@ const laptopData = JSON.parse(json);
 const server = http.createServer((req, res) => {
 
     const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
 
-    if (pathName === '/products' || pathName === '') {
+    console.log(id);
+
+    if (pathName === '/products' || pathName === '/') {
         res.writeHead(200, {'Content-type': 'text/html'});
         res.end('This is the Products page.');
     }
 
-    else if (pathName === '/laptop') {
+    else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end('This is the Laptop page.');
+        res.end(`This is the Laptop page for laptop number ${id}.`);
     }
 
     else {
-        res.writeHead(200, {'Content-type': 'text/html'});
+        res.writeHead(404, {'Content-type': 'text/html'});
         res.end('URL was not found on the server.');
     }
-    
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
